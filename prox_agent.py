d58053b1380216446de20064d014c7c120be6076
Eric Tang <eltang@cs.utexas.edu>
2019-12-12 12:05:18 -0600

    Fix ProbAgent and clean up ProxAgent

--- prox_agent.py
+++ prox_agent.py
@@ -11,21 +11,18 @@ class ProxAgent(prob_agent.ProbAgent):
     """
     def choose_next_loc(self):
         best_node = None
+        best_node_path = None
         best_node_score = 0
-        best_node_dist = 0
         for node in self.world.graph.nodes:
             if not self.visited(node):
-                score = 0
-                pot_objs = self.arrangement_space.pot_objs_at(node)
                 path = self.world.graph.shortest_path(self.loc, node)
-                # Node is scored based on aggregate probability of novel finds
-                for pot_obj in pot_objs:
-                    score += self.arrangement_space.prob_obj(pot_obj, node)
+                score = self.arrangement_space.prob_any_obj(node)
+                # Node is scored based on probability of finding any object
                 if best_node is None or \
-                   (score > 0 and path.cost < best_node_dist) or \
-                   (path.cost == best_node_dist and score > best_node_score):
+                    (score and path.cost < best_node_path.cost) or \
+                    (path.cost == best_node_path.cost and score > best_node_score):
                     best_node = node
                     best_node_score = score
-                    best_node_dist = path.cost
+                    best_node_path = path
         assert best_node is not None
-        return best_node
+        return best_node_path.nodes[1]
